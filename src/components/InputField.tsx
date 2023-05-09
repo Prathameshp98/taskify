a6b7d894e6ca91044bd44b495e5d598e2068af48
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-05-09 23:45:06 +0530

    inputs styling and states

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import './styles.css'
 
-const InputField: React.FC = () => {
+interface Props {
+    todo: string | number;
+    setTodo: React.Dispatch<React.SetStateAction<string | number>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
     <form className='input'>
-        <input type='text' className='input__box' placeholder='Enter a task' />
-        <button type='submit' className='input__submit'>GO</button>
+        <input 
+            type='text' 
+            className='input__box' 
+            placeholder='Enter a task'
+            value={todo}
+            onChange={
+                (e) => {
+                    setTodo(e.target.value)
+                }
+            } 
+        />
+        <button 
+            type='submit' 
+            className='input__submit'>GO</button>
     </form>
   )
 }
